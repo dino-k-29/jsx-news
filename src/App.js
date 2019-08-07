d8846e9f674f447292629cf0de55af09bab3b107
Dino <dino@oat-bot.com>
2019-08-07 09:12:45 +0100

    Render new posts with nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PostsList from './PostsList'
+import PropTypes from 'prop-types'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import { fetchMainPosts } from './utils/api';
 
-class App extends React.Component {
+class Posts extends React.Component {
+  static propTypes = {
+    type: PropTypes.string.isRequired
+  }
+
   state = {
     posts: null
   }
 
   componentDidMount () {
-    fetchMainPosts('top')
+    fetchMainPosts(this.props.type)
       .then(posts => this.setState({posts: posts}))
   }
 
@@ -16,9 +22,48 @@ class App extends React.Component {
     const { posts } = this.state;
 
     return (
-      <div className="container">
+      <div>
         {posts ? <PostsList posts={posts} /> : 'Loading...'}
       </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render () {
+    const activeStyle = {
+      color: 'rgb(187, 46, 31)'
+    }
+
+    return (
+      <Router>
+        <div className="container">
+          <nav className="row space-between">
+            <ul className="row nav">
+              <li>
+                <NavLink
+                  className="nav-link"
+                  activeStyle={activeStyle}
+                  exact
+                  to="/">
+                  Top
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  className="nav-link"
+                  activeStyle={activeStyle}
+                  to="/new">
+                  New
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Route exact path="/" render={() => <Posts type='top' />} />
+          <Route path="/new" render={() => <Posts type='new' />} />
+        </div>
+      </Router>
     );
   }
 }
